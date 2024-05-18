c5f3d961dde9d8d90e5738fae4f912ebcee57b39
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-05-18 14:09:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages # type: ignore
 
 setup(
-  name='GetWeather',
-  version='1.0',
-  packages=find_packages(),
-  install_requires=[
-    request
-  ],
+    name='WeatherPlug',
+    version='1.0.2',
+    packages=find_packages(),
+    install_requires=[
+        
+        'Requests>=2.31.0'
+    ],
 )
