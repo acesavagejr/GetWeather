8dee7b33272bdf5dc7aa064141758ba3603aff81
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-05-18 14:09:48 -0700

    Update Main.py

--- GetWeather/Main.py
+++ GetWeather/Main.py
@@ -1,13 +1,13 @@
-def GetWeather(city):
+def WeatherPlug(city):
   import requests
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=16d123a52e5c07ef13e101ad033248ea&units=metric'.format(city)
+  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=16d123a52e5c07ef13e101ad033248ea&units=metric'.format(city)
 
-res = requests.get(url)
-data = res.json()
-
-humidity = data['main']['humidity']
-pressure = data['main']['pressure']
-wind = data['wind']['speed']
-description = data['weather'][0]['description']
-temp = data['main']['temp']
+  res = requests.get(url) # type: ignore
+  data = res.json()
+  
+  WeatherPlug.temp = data['main']['temp']
+  WeatherPlug.temp = (WeatherPlug.temp * 9/5) + 32
+  WeatherPlug.wind = data['wind']['speed']
+  WeatherPlug.desc = data['weather'][0]['description']
+  WeatherPlug.humidity = data['main']['humidity']
